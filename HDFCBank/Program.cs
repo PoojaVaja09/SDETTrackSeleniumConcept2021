5af763a5a2c548709ca727381c2f1d571ee98b2d
poojavaja84 <pooja.vaja@gmail.com>
2021-07-17 18:21:03 -0700

    Added HDFC project

---
+++ HDFCBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace HDFCBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://netbanking.hdfcbank.com/netbanking/";
+
+            driver.SwitchTo().Frame("login_page");
+            driver.FindElement(By.Name("fldLoginUserId")).SendKeys("abs");
+
+            //click on continue
+            driver.FindElement(By.XPath("(//img[@alt='continue'])[1]")).Click();
+
+            //Comeout of frame
+
+            driver.SwitchTo().DefaultContent();
+
+        }
+    }
+}
