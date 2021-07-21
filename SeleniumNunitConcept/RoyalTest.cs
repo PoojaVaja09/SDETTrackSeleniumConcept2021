6af578fed24857627fb610fa1f812d2922e309cb
poojavaja84 <pooja.vaja@gmail.com>
2021-07-21 16:45:28 -0700

    Dynamic Dropdown in RoyalTest

---
+++ SeleniumNunitConcept/RoyalTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumNunitConcept
+{
+    class RoyalTest
+    {
+
+        [Test]
+        public void CreateAccountTest()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://www.royalcaribbean.com/";
+
+            if (driver.FindElements(By.XPath("//*[@class='email-capture__close']")).Count > 0)
+            {
+                driver.FindElement(By.XPath("//*[@class='email-capture__close']")).Click();
+            }
+
+
+            driver.FindElement(By.Id("rciHeaderSignIn")).Click();
+            driver.FindElement(By.LinkText("Create an account")).Click();
+
+            driver.FindElement(By.XPath("//input[@data-placeholder='First name/Given name']")).SendKeys("Denis");
+            driver.FindElement(By.XPath("//input[@data-placeholder='Last name/Surname']")).SendKeys("Rich");
+
+            //month april   
+            string month = "December";
+            driver.FindElement(By.XPath("//span[text()='Month']")).Click();
+            driver.FindElement(By.XPath("//span[text()=' " + month + " ']")).Click();
+
+            //30
+            driver.FindElement(By.XPath("//span[text()='Day']")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("//span[text()=' 30 ']")).Click();
+
+            //year 1990
+            driver.FindElement(By.XPath("//input[@data-placeholder='Year']")).SendKeys("1990");
+
+            //country India
+            driver.FindElement(By.XPath("//span[text()='Country/Region of residence']")).Click();
+            driver.FindElement(By.XPath("//span[text()=' India ']")).Click();
+
+            driver.FindElement(By.XPath("//input[@aria-labelledby='agreements']/..")).Click();
+        }
+    }
+}
--- SeleniumNunitConcept/SeleniumAdvance.cs
+++ SeleniumNunitConcept/SeleniumAdvance.cs
@@ -1,6 +1,10 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace SeleniumNunitConcept
@@ -8,9 +12,130 @@ namespace SeleniumNunitConcept
     class SeleniumAdvance
     {
         [Test]
+        public void HandleAlertExplcitWaitTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "http://www.echoecho.com/javascript4.htm";
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+            wait.Until(x => x.FindElement(By.Name("B1"))).Click();
+
+            //driver.FindElement(By.Name("B1")).Click();
+
+            string alertText = wait.Until(x => x.SwitchTo().Alert()).Text;
+
+            wait.Until(x => x.SwitchTo().Alert()).Accept();
+
+            Console.WriteLine(alertText);
+
+            String text = driver.FindElement(By.XPath("//div[@class='headline']")).Text;
+            Console.WriteLine(text);
+
+            //wait.Until(driver => driver.FindElement(By.XPath("//div[@class='headline']")));
+            text = wait.Until(x => x.FindElement(By.XPath("//div[@class='headline']"))).Text;
+            Console.WriteLine(text);
+
+            //driver.FindElement(By.Id("")).SendKeys("2021-07-19");
+        }
+
+        [Test]
+        public void HandleAlertTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "http://www.echoecho.com/javascript4.htm";
+
+            driver.FindElement(By.Name("B1")).Click();
+
+            string alertText = driver.SwitchTo().Alert().Text;
+
+            driver.SwitchTo().Alert().Accept();
+
+            Console.WriteLine(alertText);
+        }
+
+        [Test]
+        public void MultipleTabsOrWindowsTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://www.db4free.net/";
+
+            driver.FindElement(By.XPath("//b[contains(text(),'phpMy')]")).Click();
+
+            //switch to new tab
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+            driver.FindElement(By.Id("input_username")).SendKeys("hello123");
+            driver.FindElement(By.Id("input_password")).SendKeys("hello123");
+            driver.FindElement(By.Id("input_username")).Submit();
+
+        }
+
+        [Test]
+        public void MultipleTabsOrWindowsPrintSessionDetail()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://www.db4free.net/";
+
+            driver.FindElement(By.XPath("//b[contains(text(),'phpMy')]")).Click();
+
+            var parent = driver.CurrentWindowHandle;
+            Console.WriteLine(parent);
+            Console.WriteLine("-----------------------");
+            ReadOnlyCollection<string> windows = driver.WindowHandles;
+
+            Console.WriteLine(windows[0]);
+            Console.WriteLine(windows[1]);
+
+            for (int i = 0; i < windows.Count; i++)
+            {
+                Console.WriteLine(windows[i]);
+            }
+
+            foreach (string win in windows)
+            {
+                Console.WriteLine(win);
+            }
+
+            foreach (var win in windows)
+            {
+                Console.WriteLine(win);
+            }
+        }
+
+
+
+        [Test]
         public void ValidCredentialTest()
         {
-            Console.WriteLine("Pooja");
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://magento.com/";
+
+            driver.FindElement(By.XPath("//span[text()='Sign in']")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("balaji0017@gmail.com");
+            driver.FindElement(By.Id("pass")).SendKeys("balaji@12345");
+            driver.FindElement(By.Name("send")).Click();
+
+            //condition 
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+            wait.Until(x => x.FindElement(By.LinkText("Log Out")));
+
+            string actualValue = driver.Title;
+            Console.WriteLine(actualValue);
+
+            //verification
+            Assert.AreEqual("Account Information", actualValue);
+
         }
+
     }
 }
